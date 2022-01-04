9eeb2d290b1cd30a2b92ff4adc92af9b6cdf9844
kekecoder <abimbola.felix@gmail.com>
2022-01-04 18:36:51 +0100

    created login and registration page

--- noteapp/index.php
+++ noteapp/index.php
@@ -2,14 +2,15 @@
 <html lang="en">
   <head>
     <meta charset="utf8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <title>Note App</title>
   </head>
   <body>
     <div class="container">
-      <h2>NoteApp</h2>
+      <h2 class="mt-3">NoteApp</h2>
       <p>Welcome to your NoteApp, a place where you can keep notes of any records</p>
-      <button class="btn btn-primary">Login</button>
-      <button class="btn btn-secondary">Register</button>
+      <a href="login.php" class="btn btn-primary">Login</a>
+      <a href="register.php" class="btn btn-secondary">Register</a>
     </div>
   </body>
 </html>
---
+++ noteapp/login.php
@@ -0,0 +1,28 @@
+<!Doctype HTML>
+<html lang="en">
+  <head>
+    <meta charset="utf8">
+    <title>NoteApp | Login</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/noteapp.css">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <h2 class="text-center mt-4">Login Here</h2>
+      <form class="form mt-4">
+  <div class="form-group">
+    <label for="Email">Email</label>
+    <input type="email" class="form-control" id="email" name="email">
+    <small class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="Password">Password</label>
+    <input type="password" class="form-control" id="password" name="password">
+    <small class="form-text text-muted">We'll never share your email with anyone else.</smal>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+      <p class="mt-3">Already have an account? <a href="register.php">Signup Here</a></p> 
+    </div>
+  </body>
+</html>
